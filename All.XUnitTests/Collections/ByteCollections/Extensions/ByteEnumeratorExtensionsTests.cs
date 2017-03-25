be3bb4a411f704222c3ca91e5f44c7a2f82c4511
RobbertAbrahamse <info@ict-werkt.nl>
2017-03-25 23:20:45 +0100

    Added new tests

--- All.XUnitTests/Collections/ByteCollections/Extensions/ByteEnumeratorExtensionsTests.cs
+++ All.XUnitTests/Collections/ByteCollections/Extensions/ByteEnumeratorExtensionsTests.cs
@@ -25,5 +25,41 @@ namespace All.XUnitTests.Collections.ByteCollections.Extensions
             e.MoveNext().ShouldBe(true);
             e.GetInt32().ShouldBe(int.MinValue);
         }
+
+        [Fact]
+        public void TestGetInt64MaxValue()
+        {
+            var bytes = BitConverter.GetBytes(Int64.MaxValue);
+            var e = bytes.GetEnumerator<byte>();
+            e.MoveNext().ShouldBe(true);
+            e.GetInt64().ShouldBe(Int64.MaxValue);
+        }
+
+        [Fact]
+        public void TestGetInt64MinValue()
+        {
+            var bytes = BitConverter.GetBytes(Int64.MinValue);
+            var e = bytes.GetEnumerator<byte>();
+            e.MoveNext().ShouldBe(true);
+            e.GetInt64().ShouldBe(Int64.MinValue);
+        }
+
+        [Fact]
+        public void TestGetCharUtf16MaxValue()
+        {
+            var bytes = BitConverter.GetBytes(char.MaxValue);
+            var e = bytes.GetEnumerator<byte>();
+            e.MoveNext().ShouldBe(true);
+            e.GetCharUtf16().ShouldBe(char.MaxValue);
+        }
+
+        [Fact]
+        public void TestGetCharUtf16MinValue()
+        {
+            var bytes = BitConverter.GetBytes(char.MinValue);
+            var e = bytes.GetEnumerator<byte>();
+            e.MoveNext().ShouldBe(true);
+            e.GetCharUtf16().ShouldBe(char.MinValue);
+        }
     }
 }
